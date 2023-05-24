1b585dc8877a224a11a8f5d00e0621050e202fec
Yudha Rizqia <Yudharizqia182@gmail.com>
2023-05-25 06:32:40 +0700

    update

--- Pertemuan 8/Teori/Fungsi Recursive/main.cpp
+++ Pertemuan 8/Teori/Fungsi Recursive/main.cpp
@@ -8,41 +8,55 @@
 
 using namespace std;
 
-int penjumlahan(int a){
-    if(a==1){
-        return 1;
+int penjumlahan(int a, int b){
+    if(a<=0 && b<=0){
+        return 0;
     }
-    else{
-        return a+penjumlahan(a-1);
+    else if(a > 0){
+        return 1 + penjumlahan(a-1, b);
+    }
+    else if(b > 0){
+        return 1 + penjumlahan(a, b-1);
+    }
+
+}
+
+int pengurangan(int a, int b) {
+    if (a <= 0 && b <= 0) {
+        return 0;
+    }
+    else if (a > 0) {
+        return 1 + pengurangan(a - 1, b);
+    }
+    else if (b > 0) {
+        return -1 + pengurangan(a, b - 1);
     }
 }
 
-int pengurangan(int a){
-    if(a==1){
-        return 1;
+int perkalian(int a,int b){
+    if (a <= 0 || b <= 0) {
+        return 0;
     }
-    else{
-        return a-pengurangan(a-1);
+	else if (a > 0) {
+        return a + perkalian(a, b - 1);
+	}
+	else if (b > 0) {
+        return (-a) + perkalian(a, b+1);
+	}
+}
+
+int pembagian(int a, int b) {
+    if (a <= 0 || b <= 0) {
+        return 0;
+    }
+    else if (a < b) {
+        return 0;
+    }
+    else {
+        return 1 + pembagian(a-b, b);
     }
 }
 
-int perkalian(int a){
-    if(a==0){
-        return 1;
-    }
-    else{
-        return a*perkalian(a-1);
-    }
-}
-
-int pembagian(int a){
-    if(a==0){
-        return 1;
-    }
-    else{
-        return a/pembagian(a-1);
-    }
-}
 
 int pangkat(int a, int b)
 {
@@ -76,25 +90,28 @@ int faktorial(int a){
 
 int main()
 {
-    cout << "=== Program Rekursif Penjumlahan, Pengurangan, Perkalian, Pembagian, Fibonaci, Faktorial ===" << endl;
-    int n, x, y;
-    cout << "Masukkan Bilangan : ";
-    cin >> n;
-    cout << "Penjumlahan : " << penjumlahan(n) << endl;
-    cout << "Pengurangan : " << pengurangan(n) << endl;
-    cout << "Perkalian : " << perkalian(n) << endl;
-    cout << "Pembagian : " << pembagian(n) << endl;
-    cout << "Fibonaci : " << fibonaci(n) << endl;
-    for(int i=0; i<=n ;i++){
+    cout << "" << endl;
+    int x, y;
+    cout << "Masukkan Bilangan Pertama: ";
+    cin >> x;
+    cout << "Masukkan Bilangan Kedua: ";
+    cin >> y;
+    cout << "Penjumlahan dari bilangan " << x << " dan " << y << " : " << penjumlahan(x,y) << endl;
+    cout << "Pengurangan dari bilangan " << x << " dan " << y << " : " << pengurangan(x,y) << endl;
+    cout << "Perkalian dari bilangan " << x << " dan " << y << " : " << perkalian(x,y) << endl;
+    cout << "Pembagian dari bilangan " << x << " dan " << y << " : " << pembagian(x,y) << endl;
+    cout << "Fibonaci index ke-" << x << " : " << fibonaci(x) << endl;
+    for(int i=0; i<=x ;i++){
         cout << fibonaci(i) << " ";
     }
     cout << endl;
-    cout << "Faktorial : " << faktorial(n) << endl;
-    cout << "=== Program Rekursif Pangkat ===" << endl;
-    cout << "Masukkan Bilangan : ";
-    cin >> x;
-    cout << "Masukkan Pangkat : ";
-    cin >> y;
+    cout << "Fibonaci index ke-" << y << " : " << fibonaci(y) << endl;
+    for(int i=0; i<=y ;i++){
+        cout << fibonaci(i) << " ";
+    }
+    cout << endl;
+    cout << "Faktorial bilangan " << x << " : " << faktorial(x) << endl;
+    cout << "Faktorial bilangan " << y << " : " << faktorial(y) << endl;
     cout << "Pangkat : " << pangkat(x, y) << endl;
     return 0;
 }
